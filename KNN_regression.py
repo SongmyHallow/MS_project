ea8a7cfeb4c8e2cbbc62d1f43b82917e133f4899
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-03-28 15:09:51 -0400

    update

--- KNN_regression.py
+++ KNN_regression.py
@@ -9,29 +9,25 @@ y = np.exp(X).ravel()
  
 #add noise
 y += 0.1 * np.random.rand(n_dots) - 0.1
-# plt.plot(X,y, 'r.')
-# plt.show()
-
-#KNN Regression
-k = 2
-knn = KNeighborsRegressor(k)
-knn.fit(X,y)
-prec = knn.score(X, y)  #计算拟合曲线针对训练样本的拟合准确性
-print(prec)
- 
-#generate enough predict data
-T = np.linspace(0, 5, 500)[:, np.newaxis]
-y_pred = knn.predict(T)
- 
-#draw regress curve
-plt.figure(figsize=(16, 10), dpi = 144)
-plt.scatter(X, y, c='g', label='data', s=100) #训练样本
-plt.scatter(T, y_pred, c='k', label='prediction', lw=2) #拟合曲线
-plt.axis('tight')
-plt.title('KNN regression (k =%i)'%k)
-# plt.title()
-# plt.show()
-# #print(X)
-# #print(y)
- 
+plt.plot(X,y, 'r.')
 plt.show()
+
+# #KNN Regression
+# k = 2
+# knn = KNeighborsRegressor(k)
+# knn.fit(X,y)
+# prec = knn.score(X, y)  #计算拟合曲线针对训练样本的拟合准确性
+# print(prec)
+
+# #generate enough predict data
+# T = np.linspace(0, 5, 500)[:, np.newaxis]
+# y_pred = knn.predict(T)
+ 
+# #draw regress curve
+# plt.figure(figsize=(16, 10), dpi = 144)
+# plt.scatter(X, y, c='g', label='data', s=100) #训练样本
+# plt.scatter(T, y_pred, c='k', label='prediction', lw=2) #拟合曲线
+# plt.axis('tight')
+# plt.title('KNN regression (k =%i)'%k)
+ 
+# plt.show()
--- SALib_test.py
+++ SALib_test.py
@@ -2,9 +2,6 @@ from SALib.sample import saltelli
 from SALib.analyze import sobol
 from SALib.test_functions import Ishigami
 import numpy as np
-
-import sobol_seq
-
 problem = {
   'num_vars': 3,
   'names': ['x1', 'x2', 'x3'],
