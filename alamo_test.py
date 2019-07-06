d9fe8977f6edbb592e1aeb113092c6d18db22f3a
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-07-06 19:33:32 -0400

    update coordinate search

--- alamo_test.py
+++ alamo_test.py
@@ -3,17 +3,15 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import examples
-ndata=30
-# x = np.random.uniform([-2,-1],[2,1],(ndata,2))
-x = np.random.random((30,20))
-z = [0]*ndata
+from sklearn.model_selection import train_test_split
 
-sim = examples.sixcamel
-for i in range(ndata):
-    z[i] = sim(x[i][0],x[i][1])
-almsim = alamopy.wrapwriter(sim)
-# res = alamopy.doalamo(x,z,almname='cam6',monomialpower=(1,2,3,4,5,6),multi2power=(1,2),simulator=almsim, expandoutput=True,maxiter=20,cvfun=True)
-res = alamopy.doalamo(x,z,almname='cam6',monomialpower=(1,2,3,4),multi2power=(1,2),simulator=almsim,expandoutput=True,maxiter=20)
+# X_train = [[-5], [-1], [-3], [3], [2], [1], [-4], [5]]
+
+X_train = [-5,-1,-3,3,2,1,-4,5]
+y_train = [25, 1, 9, 9, 4, 1, 16, 25]
+X_test = [[-2], [0], [4]]
+y_test = [4, 0, 16]
+# X_train,X_test,y_train,y_test=train_test_split(xdata,ydata,test_size=0.25)
+print(X_train,X_test,y_train,y_test)
+res = alamopy.alamo(xdata=X_train,zdata=y_train,xval=X_test,zval=y_test,xmax=5,xmin=-5,monomialpower=(1,2),showalm=True)
 print(res['model'])
-
-expression = res['model']
