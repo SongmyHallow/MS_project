12cc55bff8ae99f8a968694bd0ae6b48fc3e08e2
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-07-07 18:43:23 -0400

    apply shuffle order and enable csv output Van der Corput & coordinate search

--- clear.sh
+++ clear.sh
@@ -9,54 +9,53 @@ File5="/mnt/c/Users/tjuso/Documents/Graduate/MS_project/cam6"
 File6="/mnt/c/Users/tjuso/Documents/Graduate/MS_project/input.in"
 File7="/mnt/c/Users/tjuso/Documents/Graduate/MS_project/output.out"
 
-# localFile=$"/mnt/c/Users/tjuso/Documents/Graduate/MS_project/z1.py"
 
 if [ -f "$File1" ];then
     sudo rm -f "$File1"
-    echo $File1"\\tis deleted"
+    echo $File1" is deleted"
 else
-    echo $File1"\\tdoes not exist"
+    echo $File1" does not exist"
 fi
 
 if [ -f "$File2" ];then
     sudo rm -f "$File2"
-    echo $File2"\\tis deleted"
+    echo $File2" is deleted"
 else
-    echo $File2"\\tdoes not exist"
+    echo $File2" does not exist"
 fi
 
 if [ -f "$File3" ];then
     sudo rm -f "$File3"
-    echo $File3"\\tis deleted"
+    echo $File3" is deleted"
 else
-    echo $File3"\\tdoes not exist"
+    echo $File3" does not exist"
 fi
 
 if [ -f "$File4" ];then
     sudo rm -f "$File4"
-    echo $File4"\\tis deleted"
+    echo $File4" is deleted"
 else
-    echo $File4"\\tdoes not exist"
+    echo $File4" does not exist"
 fi
 
 if [ -f "$File5" ];then
     sudo rm -f "$File5"
-    echo $File5"\\tis deleted"
+    echo $File5" is deleted"
 else
-    echo $File5"\\tdoes not exist"
+    echo $File5" does not exist"
 fi
 
 if [ -f "$File6" ];then
     sudo rm -f "$File6"
-    echo $File6"\\tis deleted"
+    echo $File6" is deleted"
 else
-    echo $File6"\\tdoes not exist"
+    echo $File6" does not exist"
 fi
 if [ -f "$File7" ];then
     sudo rm -f "$File7"
-    echo $File7"\\tis deleted"
+    echo $File7" is deleted"
 else
-    echo $File7"\\tdoes not exist"
+    echo $File7" does not exist"
 fi
 
 find . -name "*.exe" | xargs rm -rf
