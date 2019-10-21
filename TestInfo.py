b7a3b6098e58d0348b157bc4a4e42287049e1739
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-10-21 14:43:05 -0400

    update reference use script to gather model information in xlsx

---
+++ TestInfo.py
@@ -0,0 +1,49 @@
+'''
+This script is used to gather information from nik's webiste:
+'''
+import requests
+from bs4 import BeautifulSoup
+import xlsxwriter as wx
+import sys
+import xlrd, xlwt
+
+def web_info():
+    response = requests.get('http://thales.cheme.cmu.edu/dfo/comparison/comp.html')
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    title = []
+    sol = []
+    for index, x in enumerate(soup.find_all('tr')):
+        text = x.get_text().split()
+        if index == 0:
+            title = text[:]
+            title.insert(0,"")
+        else:
+            sol.append(text)
+    return title,sol
+
+def set_style(name,height,bold=False):
+	style = xlwt.XFStyle()
+	font = xlwt.Font()
+	font.name = name
+	font.bold = bold
+	font.color_index = 4
+	font.height = height
+	style.font = font
+	return style
+
+def write_excel(title,sol):
+	f = xlwt.Workbook()
+	sheet1 = f.add_sheet('models',cell_overwrite_ok=True)
+	row0 = title
+	#写第一行
+	for i in range(0,len(row0)):
+		sheet1.write(0,i,row0[i],set_style('Times New Roman',220,True))
+	for index,row in enumerate(sol):
+		for i in range(0,len(row)):
+			sheet1.write(index+1,i,row[i])
+	f.save('ModleList.xls')
+
+if __name__ == "__main__":
+    title, sol = web_info()
+    write_excel(title,sol)
