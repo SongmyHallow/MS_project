d2c7569f324b5e00d2e91b064973a98f1ea0b207
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-10-26 21:35:38 -0400

    update HYSYS module

--- BlackBoxCore.py
+++ BlackBoxCore.py
@@ -65,7 +65,7 @@ def genBlackBoxValuesSeq(filename,point,sequence,index):
         os.system('.\\'+filename)
         output_values.append(readOutput(output_filename))
     return output_values
-
+    
 def boundary_dic(labels,lb,ub):
     lowerBound = {labels[0]:lb}
     upperBound = {labels[0]:ub}
@@ -454,12 +454,12 @@ class blackBox(object):
                         self.samples[indexOfVar] = int(self.samples[indexOfVar]*0.8)
                         self.allCalls.append(self.totalCalls)
                         self.allValue.append(boxVal)
-            if(self.checkEnd()==True or self.totalCalls > 5000):
-                print("Exit normally")
-                ref_coordiante = self.readReferenceSol()
-                ref_optimal = self.readRefOptimal()
-                self.error = np.abs(ref_optimal-self.allValue[-1])/self.allValue[-1]
-                return
+                    if(self.checkEnd()==True or self.totalCalls > 5000):
+                        print("Exit normally")
+                        ref_coordiante = self.readReferenceSol()
+                        ref_optimal = self.readRefOptimal()
+                        self.error = np.abs(ref_optimal-self.allValue[-1])/self.allValue[-1]
+                        return
 
     def coordinateSearchBeta(self):
         # Initialization
@@ -493,6 +493,195 @@ class blackBox(object):
                 return
 
 
+def HYSYSsimulation(cycles):
+    import os
+    import win32com.client as win32
+    import numpy as np
+    import time
+    
+    def hy_distinguish(hysolver):
+        i=4
+        P=np.linspace(0,0,num=1001)
+        k=0
+        timeover=0
+        while k<2:
+            i+=1
+            solveboolean = 1-hysolver.issolving
+            P[i] = solveboolean
+            k = P[i-2] + P[i-1] + P[i]
+            time.sleep(1)
+            if P[1000]==1:
+                timeover=1
+                break
+        return timeover	
+    
+    def hy_Object(hyCase, hysolver, variable):
+        error=0
+        error_type = np.array([0,0,0])
+        butane = 100 - (variable[4] + variable[5] + variable[6] + variable[7])
+        if variable[0]<=variable[1] and variable[1]<=variable[2] and variable[2]<=variable[3] and variable[4] >= 0:
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C2').cellvalue = variable[0]*100   #LP multiply 100 for making unit as bar
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C3').cellvalue = variable[1]*100   #MP1
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C4').cellvalue = variable[2]*100   #MP2
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C5').cellvalue = variable[3]*100   #HP
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C6').cellvalue = variable[4]   	  #Nitrogen
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C7').cellvalue = variable[5]       #Methane
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C8').cellvalue = variable[6]       #Ethane
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C9').cellvalue = variable[7]       #propane
+            hyCase.Flowsheet.operations.Item('optimization').Cell('C10').cellvalue = butane    
+            timeover=hy_distinguish(hysolver)
+            # --there are infeasible signs?
+            try:
+                V1=hyCase.Flowsheet.operations.Item('optimization').Cell('I2').cellvalue
+                V2=hyCase.Flowsheet.operations.Item('optimization').Cell('I3').cellvalue
+                V3=hyCase.Flowsheet.operations.Item('optimization').Cell('I4').cellvalue
+                V4=hyCase.Flowsheet.operations.Item('optimization').Cell('I5').cellvalue
+                V5=hyCase.Flowsheet.operations.Item('optimization').Cell('I6').cellvalue
+            except:
+                hyCase.Flowsheet.operations.Item('optimization').Cell('C11').cellvalue = 3
+                timeover=hy_distinguish(hysolver)
+                error_type[1]=1
+                if np.dot(error_type,error_type) !=0:
+                    error=1
+                    OBJ = np.random.random_sample()*10**30
+                    return OBJ
+                else:
+                    pass
+            
+            if abs(V4-V5)>0.1:
+                error_type[1]=1
+                error =1
+                OBJ = np.random.random_sample()*10**30
+                return OBJ
+
+            MTD_HX1 = hyCase.Flowsheet.operations.Item('optimization').Cell('C14').cellvalue
+            MTD_HX2 = hyCase.Flowsheet.operations.Item('optimization').Cell('C16').cellvalue
+
+            if MTD_HX1 <=2.85 or MTD_HX2<=2.85:
+                hyCase.Flowsheet.operations.Item('optimization').Cell('C11').cellvalue = 3
+                timeover=hy_distinguish(hysolver)
+                if MTD_HX1 <=2.85 or MTD_HX2<=2.85:
+                    error_type[0]=1
+                    if np.dot(error_type,error_type) !=0:
+                        error=1
+                        OBJ = np.random.random_sample()*10**30
+                        return OBJ
+            
+            VF1 = hyCase.Flowsheet.operations.Item('optimization').Cell('I8').cellvalue
+            VF2 = hyCase.Flowsheet.operations.Item('optimization').Cell('I9').cellvalue
+            VF3 = hyCase.Flowsheet.operations.Item('optimization').Cell('I10').cellvalue
+            VF4 = hyCase.Flowsheet.operations.Item('optimization').Cell('I11').cellvalue
+            VF5 = hyCase.Flowsheet.operations.Item('optimization').Cell('I12').cellvalue
+
+            if VF1!=1 or VF2!=1 or VF3!=1 or VF4!=1 or VF5 !=1:
+                error_type[2]=1
+                if np.dot(error_type,error_type) !=0:
+                    error =1
+                    OBJ = np.random.random_sample()*10**30
+                    return OBJ
+
+            # -- Transfer output variables (HYSYS --> python)
+            OBJ  = hyCase.Flowsheet.operations.Item('optimization').Cell('G10').cellvalue # E Total Energy Consumption / LNG Production(Ton per day)
+            return OBJ
+        else:
+            error=1
+            OBJ = np.random.random_sample()*10**30
+            return OBJ
+
+    def genHYSYSValuesSeq(point,sequence,index,hyCase,hysolver):
+        ydata = []
+        for val in sequence:
+            input_copy = point[:]
+            input_copy[index] = val
+            f_x0 = hy_Object(hyCase, hysolver, input_copy)
+            ydata.append(f_x0)
+        return ydata
+
+    def updateHYSYSflag(box,tempPoint,tempMinimal,indexOfVar,hyCase,hysolver):
+        import numpy as np
+        boxVal = hy_Object(hyCase, hysolver, tempPoint)
+        # print("Box value:",boxVal)
+        # print("Temp minimal value:",tempMinimal)
+        if(boxVal == 0):
+            boxVal += 1e-5
+        ratio = tempMinimal / boxVal
+        if(ratio > 0.75 and ratio < 1.5):
+            if(len(box.minimalValue)<1 or boxVal<box.minimalValue[-1]):
+                box.actualStart = tempPoint
+                box.minimalValue.append(boxVal)
+                box.minimalCoordinate.append(tempPoint)
+                box.calls.append(box.totalCalls)
+            box.radius[indexOfVar] *= 3
+            print("Raidus is increased to: ",box.radius[indexOfVar])
+            return True,boxVal
+        else:
+            box.radius[indexOfVar] *= 0.8
+            print("Raidus is decreased to: ",box.radius[indexOfVar])
+            return False,boxVal
+
+    def resetAspen():
+        from pynput.mouse import Button, Controller
+        # Get the mouse controlled
+        mouse = Controller()
+        # The current coordinate of mouse
+        print(mouse.position)
+        # Mouse with the new coordinate
+        mouse.position = (784, 705)
+        # left click
+        mouse.click(Button.left, 1)
+
+    '''maximum number of function evaluation is 2200'''
+    ''' Connecting to the Aspen Hysys App just one time during optimization'''      
+    print(' # Connecting to the Aspen Hysys App ... ')
+    hyapp    = win32.Dispatch('HYSYS.Application')			   # Connecting to the Application
+    hyCase   = hyapp.ActiveDocument                          # Access to active document
+    hysolver = hyCase.Solver
+
+    box = blackBox(name='HYSYS',cycles=cycles)
+    box.lowBound=np.array([0.3000,0.7500,1.8750,4.6750,0.8590,2.5970,2.5410,3.9110])
+    box.upBound=np.array([5.7000,14.2500,35.6250,88.8250,16.3210,49.3430,48.2790,74.3090])
+    box.iniStart = (box.lowBound+box.upBound)/2
+    box.genActualStart("origin")
+    box.numOfVar = 8
+
+    # Initialization
+    ini_sample = 4
+    box.samples = [ini_sample for i in range(box.numOfVar)]
+    ini_radius = 1
+    box.radius = [ini_radius for i in range(box.numOfVar)]
+
+    for cycle in range(box.cycles):
+        print("The No.",cycle+1,"Cycle")
+        for indexOfVar in range(box.numOfVar):
+            print("The No.",indexOfVar+1,"Variable")
+
+            lb,ub = box.genVariableBound(indexOfVar)
+            Xdata = box.genSamplePoints("vander",box.samples[indexOfVar],lb,ub)
+            print(Xdata)
+
+            # evalute sampling points
+            ydata = genHYSYSValuesSeq(box.actualStart,Xdata,indexOfVar,hyCase,hysolver)
+            labels, expr = callAlamopy(Xdata, ydata, lb, ub)
+            tempPoint, tempMinimal = box.callBaron(labels,expr, lb, ub, indexOfVar)
+
+            flag, boxVal = updateHYSYSflag(box,tempPoint,tempMinimal,indexOfVar,hyCase,hysolver)
+            
+            print("Flag: ", flag)
+            
+            if(flag == False):
+                box.samples[indexOfVar] += 8
+            else:
+                box.samples[indexOfVar] = int(box.samples[indexOfVar]/2)
+                # Store all of the output and coordinates for future inspection
+                box.allCalls.append(box.totalCalls)
+                box.allValue.append(boxVal)
+            if(box.checkEnd() == True):
+                return
+            resetAspen()
+
+    # f_x0 = hy_Object(hyCase, hysolver, box.iniStart)
+    # print('function output of x0 is: ',f_x0)
+    return
 """
 ================================================================================
 main function
@@ -503,21 +692,24 @@ if __name__ == "__main__":
     cycles = int(sys.argv[2])
 
 def main():
-    import time
-    box = blackBox(name=fileName,cycles=cycles)
-    box.compileCode()
-    box.readDataFile()
-    box.genBackupStart()
-    box.genActualStart("origin")
+    if fileName != 'HYSYS':
+        import time
+        box = blackBox(name=fileName,cycles=cycles)
+        box.compileCode()
+        box.readDataFile()
+        box.genBackupStart()
+        box.genActualStart("origin")
 
-    startTime = time.time()
-    box.coordinateSearch()
-    # box.coordinateSearchBeta()
-    endTime = time.time()
-    dur = endTime - startTime
-    # box.showParameter()
-    box.getResult()
-    box.makeCsv(time=dur)
-    # box.makePlot()
+        startTime = time.time()
+        # box.coordinateSearch()
+        # box.coordinateSearchBeta()
+        endTime = time.time()
+        dur = endTime - startTime
+        # box.showParameter()
+        # box.getResult()
+        # box.makeCsv(time=dur)
+        # box.makePlot()
+    else:
+        HYSYSsimulation(cycles)
 
 main()
